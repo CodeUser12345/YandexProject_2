c03b53ef06dc6bae454a286aabb82ee2dd984dd4
Administrator <dim.korol@list.ru>
2022-12-24 13:35:44 +0300

    Version 0.0.3

--- main.py
+++ main.py
@@ -28,8 +28,7 @@ class Sprite(pygame.sprite.Sprite):
         super().__init__()
         self.rect = pygame.Rect(x, y, width, height)
         self.set_image(self.load_image(""))
-        self.vx, self.vy, self.speed, self.vector_flag, self.speed_flag = 0, 0, 0, False, False
-        self.step_x, self.step_y, self.last_time, self.step_time = 0, 0, time.time(), 0.001
+        self.last_time, self.step_time = time.time(), 0.001
         self.animations, self.animation_frame_index, self.animation_index, self.animation_flag = [], 0, 0, False
 
     def add(self, group):
@@ -43,23 +42,6 @@ class Sprite(pygame.sprite.Sprite):
 
     def set_image(self, image):
         self.image = image
-
-    def set_vector(self, degrees):
-        if degrees is not None:
-            self.vector_flag = True
-            degrees %= 360
-            radians = math.radians(degrees)
-            self.vx = math.sin(radians) if degrees != 0 and degrees != 180 else 0
-            self.vy = math.cos(radians) if degrees != 90 and degrees != 270 else 0
-        else:
-            self.vector_flag = False
-
-    def set_speed(self, speed):
-        if speed is not None and speed != 0:
-            self.speed_flag = True
-            self.speed = speed
-        else:
-            self.speed_flag = False
 
     def set_position_x(self, x):
         self.rect.x = x
@@ -110,6 +92,65 @@ class Sprite(pygame.sprite.Sprite):
     def set_deactivate_animation(self):
         self.animation_flag = False
 
+    def _animation(self, number):
+        last_animation_frame_index = int(self.animation_frame_index)
+        self.animation_frame_index += number * self.animations[self.animation_index][2]
+        now_animation_frame_index = int(self.animation_frame_index)
+        if now_animation_frame_index > last_animation_frame_index:
+            if now_animation_frame_index < self.animations[self.animation_index][1]:
+                self.set_image(self.animations[self.animation_index][0][now_animation_frame_index])
+            else:
+                self.set_deactivate_animation()
+
+    def _get_steps_last_and_now_time(self, time_now):
+        step_last_and_now_time = time_now - self.last_time
+        number = int(step_last_and_now_time // self.step_time)
+        self.last_time = time_now - (step_last_and_now_time - number * self.step_time)
+        return number
+
+    def update(self, time_now):
+        number = self._get_steps_last_and_now_time(time_now)
+        if number:
+            if self.animation_flag:
+                self._animation(number)
+
+
+class Entity(Sprite):
+    def __init__(self, x, y, path_image):
+        super().__init__(x, y, 25, 25)
+        self.vx, self.vy, self.speed, self.vector_flag, self.speed_flag = 0, 0, 0, False, False
+        self.step_x, self.step_y = 0, 0
+        self.set_image(self.load_image(path_image, -1))
+
+        self.name = ""
+        self.species = ""
+        self.level_now, self.level_max = 0, 0
+        self.power = 0
+        self.protection = 0
+        self.speed = 0
+        self.intellect = 0
+        self.health_points_now, self.health_points_max = 0, 0
+        self.mana_points_now, self.mana_points_max = 0, 0
+        self.energy_points_now, self.energy_points_max = 0, 0
+        self.endurance_points_now, self.endurance_points_max = 0, 0
+
+    def set_vector(self, degrees):
+        if degrees is not None:
+            self.vector_flag = True
+            degrees %= 360
+            radians = math.radians(degrees)
+            self.vx = math.sin(radians) if degrees != 0 and degrees != 180 else 0
+            self.vy = math.cos(radians) * -1 if degrees != 90 and degrees != 270 else 0
+        else:
+            self.vector_flag = False
+
+    def set_speed(self, speed):
+        if speed is not None and speed != 0:
+            self.speed_flag = True
+            self.speed = speed
+        else:
+            self.speed_flag = False
+
     def _run(self, number):
         for _ in range(number):
             self.step_x += self.vx * self.speed
@@ -124,49 +165,13 @@ class Sprite(pygame.sprite.Sprite):
             self.step_y -= number
             self.rect.y += number
 
-    def _animation(self, number):
-        last_animation_frame_index = int(self.animation_frame_index)
-        self.animation_frame_index += number * self.animations[self.animation_index][2]
-        now_animation_frame_index = int(self.animation_frame_index)
-        if now_animation_frame_index > last_animation_frame_index:
-            if now_animation_frame_index < self.animations[self.animation_index][1]:
-                self.set_image(self.animations[self.animation_index][0][now_animation_frame_index])
-            else:
-                self.set_deactivate_animation()
-
-    def update(self):
-        time_now = time.time()
-        step_last_and_now_time = time_now - self.last_time
-        number = int(step_last_and_now_time // self.step_time)
-        self.last_time = time_now - (step_last_and_now_time - number * self.step_time)
-        if step_last_and_now_time >= self.step_time:
+    def update(self, time_now):
+        number = self._get_steps_last_and_now_time(time_now)
+        if number:
             if self.animation_flag:
                 self._animation(number)
             if self.speed_flag and self.vector_flag:
                 self._run(number)
-
-
-class Entity(Sprite):
-    def __init__(self, x, y, path_image):
-        super().__init__(x, y, 25, 25)
-        self.set_image(self.load_image(path_image, -1))
-        self.set_sizes(50, 50)
-        self.name = ""
-        self.species = ""
-        self.level_now, self.level_max = 0, 0
-        self.power = 0
-        self.protection = 0
-        self.speed = 0
-        self.intellect = 0
-        self.health_points_now, self.health_points_max = 0, 0
-        self.mana_points_now, self.mana_points_max = 0, 0
-        self.energy_points_now, self.energy_points_max = 0, 0
-        self.endurance_points_now, self.endurance_points_max = 0, 0
-
-    def update(self):
-        pass
-        pass
-
 
 class Button:
     def __init__(self, x, y, width, height):
@@ -199,6 +204,8 @@ class MainWindow:
 
         entity = Entity(25, 25, "data\\frames_2\\Spider_1 — копия (5).png")
         entity.add(all_sprites)
+        entity.set_vector(360)
+        entity.set_speed(0.1)
 
         running = True
         while running:
@@ -206,7 +213,8 @@ class MainWindow:
                 if event.type == pygame.QUIT:
                     running = False
 
-            entity.update()
+            time_now = time.time()
+            entity.update(time_now)
 
             screen.fill((255, 255, 255))
             all_sprites.draw(screen)
